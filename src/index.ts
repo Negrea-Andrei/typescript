495f34e345fadb9ac581f2678b2c4145cbcbfe6a
Negrea-Andrei <negrea97@yahoo.com>
2024-01-27 22:02:01 +0200

    literal types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,40 @@
-
 type Employee = {
-    readonly id: number,
-    name:string,
-    retire: (date: Date) => void
-}
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
 
 let employee: Employee = {
-    id: 1,
-    name: "John",
-    retire: (date:Date) => {
-        console.log(date)
-    }
+  id: 1,
+  name: "John",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === "number") {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
 }
+
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+type Quantity = 50 | 100;
+
+let q: Quantity = 100;
